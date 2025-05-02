59599dca7a9f4abef91c5ce72cdf4da039327862
Anuththara Sathyajith <wickramage28@gmail.com>
2025-05-02 16:14:49 +0530

    user profile local storage

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -33,6 +33,7 @@ export default function LoginPage() {
       toast.success("Login successful!");
       const user = res.data.user;
       localStorage.setItem("token", res.data.token);
+      localStorage.setItem("userEmail", user.email);
       
       if (user.role === "admin") {
         navigate("/admin/");
